79c123d31ed09086a5eb93c33f8de9e42c04b4ef
Kuvuklija <43697762+Kuvuklija@users.noreply.github.com>
2019-02-09 16:16:10 +0300

    Initial commit

---
+++ ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+using ClientFeatures.Models;
+
+namespace ClientFeatures.Controllers{
+
+    public class HomeController : Controller{
+    
+        public ViewResult MakeBooking() {
+            return View(new Appointment() {
+                ClientName ="Vadim",
+                TermsAccepted =true
+            });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appt) {
+            //statements to store new Appointment in a 
+            //repository would go here in real project
+            return Json(appt, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
